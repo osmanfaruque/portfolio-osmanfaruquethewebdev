cdb1361b14b7b41e5e784d2ceef31907f911477c
osmanfaruque <faruqueosman0@gmail.com>
2025-10-18 00:14:29 +0600

    feat: enable dynamic imports for homepage components with SSR disabled

--- app/page.js
+++ app/page.js
@@ -1,16 +1,17 @@
-import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";  
+"use client";
+import dynamic from 'next/dynamic';
+// Dynamically import components with SSR disabled
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
+const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
+const Education = dynamic(()=> import ("./components/homepage/education"), { ssr: false });
+const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
 
-export default async function Home() {
-
+export default function Home() {
   return (
-    <div suppressHydrationWarning >
+    <>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -18,6 +19,6 @@ export default async function Home() {
       <Projects />
       <Education />
       <ContactSection />
-    </div>
-  )
-};
+    </>
+  );
+}
