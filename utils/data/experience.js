ed1766c878501bb32a8df4a6fb63f118f03907e3
osmanfaruque <faruqueosman01@gmail.com>
2026-03-15 15:58:24 +0600

    fix: reorder experience entries in experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,14 @@
 export const experiences = [
   {
     id: 1,
-    title: "Intern QA Engineer",
-    company: "A1qa | Software testing company",
-    duration: "(Dec 2023 - Feb 2024)",
-  },
-  {
-    id: 2,
     title: "Junior Frontend Developer",
     company: "ScaleUp | Software development company | Onsite",
     duration: "(Nov 2025 - Present)",
+  },
+  {
+    id: 2,
+    title: "Intern QA Engineer",
+    company: "A1qa | Software testing company",
+    duration: "(Dec 2023 - Feb 2024)",
   },
 ];
